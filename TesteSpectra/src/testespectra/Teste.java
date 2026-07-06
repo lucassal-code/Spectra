9b56a46f91c0930e15386f41d7c2fd006555fa4e
lucassal-code <lucasalamaleiko@gmail.com>
2026-07-06 15:17:22 -0300

    Teste de hex da cor dominante

--- TesteSpectra/src/testespectra/Teste.java
+++ TesteSpectra/src/testespectra/Teste.java
@@ -1,74 +1,95 @@
 package testespectra;
 
-//códiog que 
+// Source - https://stackoverflow.com/a/10530562
+// Posted by Zaz Gmy
+// Retrieved 2026-07-06, License - CC BY-SA 3.0
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+
 
 public class Teste {
-    /**
-    * Dominant Color Sort I (v1.03)
-    * GoToLoop (2016-Jan-11)
-    * forum.Processing.org/two/discussion/14393/getting-the-dominant-color-of-an-image
 
-    static final int QTY = 5, VARIATION = 50;
-    final color[] dominantArr  = new color[QTY];
-    final IntDict dominantDict = new IntDict(QTY);
+    public static void main(String args[]) throws Exception {
+        File file = new File("‪C:\\Users\\0081998\\Downloads\\teste.jpg");
+        ImageInputStream is = ImageIO.createImageInputStream(file);
+        Iterator iter = ImageIO.getImageReaders(is);
 
-    IntDict sortedDict;
-    PImage pic;
-    int len;
+        if (!iter.hasNext())
+        {
+            System.out.println("Cannot load the specified file "+ file);
+            System.exit(1);
+        }
+        ImageReader imageReader = (ImageReader)iter.next();
+        imageReader.setInput(is);
 
-    void setup() {
-      size(800, 600);
-      noLoop();
+        BufferedImage image = imageReader.read(0);
 
-      pic = createImage(width, height, ARGB);
-      len = pic.pixels.length;
+        int height = image.getHeight();
+        int width = image.getWidth();
+
+        Map m = new HashMap();
+        for(int i=0; i < width ; i++)
+        {
+            for(int j=0; j < height ; j++)
+            {
+                int rgb = image.getRGB(i, j);
+                int[] rgbArr = getRGBArr(rgb);                
+                // Filter out grays....                
+                if (!isGray(rgbArr)) {                
+                        Integer counter = (Integer) m.get(rgb);   
+                        if (counter == null)
+                            counter = 0;
+                        counter++;                                
+                        m.put(rgb, counter);                
+                }                
+            }
+        }        
+        String colourHex = getMostCommonColour(m);
+        System.out.println(colourHex);
     }
 
-    void draw() {
-      background(randomPixels(pic, VARIATION));
 
-      sortedDict = countColorsIntoDict(pic.pixels);
-      sortedDict.sortValuesReverse();
+    public static String getMostCommonColour(Map map) {
+        List list = new LinkedList(map.entrySet());
+        Collections.sort(list, new Comparator() {
+              public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o1)).getValue())
+                  .compareTo(((Map.Entry) (o2)).getValue());
+              }
+        });    
+        Map.Entry me = (Map.Entry )list.get(list.size()-1);
+        int[] rgb= getRGBArr((Integer)me.getKey());
+        return Integer.toHexString(rgb[0])+" "+Integer.toHexString(rgb[1])+" "+Integer.toHexString(rgb[2]);        
+    }    
 
-      dominantDict.clear();
-      java.util.Arrays.fill(dominantArr, 0);
+    public static int[] getRGBArr(int pixel) {
+        int alpha = (pixel >> 24) & 0xff;
+        int red = (pixel >> 16) & 0xff;
+        int green = (pixel >> 8) & 0xff;
+        int blue = (pixel) & 0xff;
+        return new int[]{red,green,blue};
 
-      final int dictSize = sortedDict.size(), limit = min(QTY, dictSize);
-      println("\nUnique colors found:", dictSize, "\tfrom:", len, ENTER);
-
-      for (int i = 0; i != limit; ++i) {
-        final String k = sortedDict.key(i);
-        dominantDict.set(k, sortedDict.value(i));
-        dominantArr[i] = int(k) | #000000;
-      }
-
-      for (int i = 0; i != QTY; ++i)
-        println(i, "->", hex(dominantArr[i], 6), "\tcount:", dominantDict.value(i));
-
-      println();
-      println(dominantDict, ENTER);
-      println(dominantArr);
+  }
+    public static boolean isGray(int[] rgbArr) {
+        int rgDiff = rgbArr[0] - rgbArr[1];
+        int rbDiff = rgbArr[0] - rgbArr[2];
+        // Filter out black, white and grays...... (tolerance within 10 pixels)
+        int tolerance = 10;
+        if (rgDiff > tolerance || rgDiff < -tolerance) 
+            if (rbDiff > tolerance || rbDiff < -tolerance) { 
+                return false;
+            }                 
+        return true;
     }
-
-    void mousePressed() {
-      redraw = true;
-    }
-
-    PImage randomPixels(final PImage img, final int v) {
-      final color p[] = img.pixels, d = 0400 - min(0400, abs(v));
-
-      for (int i = 0; i != p.length; p[i++] = color(
-        (color) random(d, 0400), 
-        (color) random(d, 0400), 
-        (color) random(d, 0400)));
-
-      img.updatePixels();
-      return img;
-    }
-
-    static final IntDict countColorsIntoDict(final color... colors) {
-      final IntDict iDict = new IntDict();
-      for (color c : colors)  iDict.increment(str(c & ~#000000));
-      return iDict;
-    }*/
 }
